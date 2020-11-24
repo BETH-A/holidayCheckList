1fe1af8cef31728b6501fe34879c07973119f4cb
Beth-A <elizabeth78@me.com>
2020-11-24 15:40:34 -0500

    Initial Commit

---
+++ holidayCheckList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  holidayCheckList
+//
+//  Created by Mary Arnold on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
